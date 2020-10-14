361a08a845584575b43520e7de72acdfcd8cfd1a
Alex <alexkiyan.lug@gmail.com>
2020-10-14 22:48:01 +0300

    fix delete flashcards

--- app/Services/FlashcardService.php
+++ app/Services/FlashcardService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Flashcard;
 use App\Models\Pack;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
@@ -10,8 +11,18 @@ class FlashcardService
 {
     public function save(Request $request, Pack $pack)
     {
+        $this->delete($request, $pack);
+
         foreach ($request->flashcards as $flashcard) {
             $pack->flashcards()->updateOrCreate(['id' => Arr::get($flashcard, 'id')], $flashcard);
         }
+    }
+
+    public function delete(Request $request, Pack $pack)
+    {
+        $new = collect($request->flashcards)->pluck('id');
+        $old = $pack->flashcards->pluck('id');
+
+        Flashcard::destroy($old->diff($new));
     }
 }
