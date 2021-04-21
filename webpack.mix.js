3f0f848d2e69b0e2d804c00ad101df138276ac5b
Alex <alexkiyan.lug@gmail.com>
2021-04-21 13:37:45 +0300

    docker step 2

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix')
 
 mix
-  .copy('nuxt/dist/index.html', 'resources/views/app.blade.php')
-  .copyDirectory('nuxt/dist', 'public')
+  .copy('dist/index.html', 'resources/views/app.blade.php')
+  .copyDirectory('dist', 'public')
