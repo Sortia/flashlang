c0979e8257a0027d7983d1bf67ce5340f7e685a9
Alex <alexkiyan.lug@gmail.com>
2021-04-21 18:03:35 +0300

    test drop auth middleware build

--- client/middleware/custom_auth.js
+++ client/middleware/custom_auth.js
@@ -1,4 +1,4 @@
 export default function (params) {
-  // if (!params.req && !params.$auth.user)
-  //   return params.redirect('/login')
+  if (!params.req && !params.$auth.user)
+    return params.redirect('/login')
 }
